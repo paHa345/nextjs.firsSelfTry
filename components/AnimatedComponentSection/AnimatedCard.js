0202f85e6f8aec79d919f5b7bceb3ef904d15aac
paHa345 <pav.345@mail.ru>
2023-01-27 01:05:30 +0700

    ver 1.39 by 27.01 fix main kayout display in small resolution

---
+++ components/AnimatedComponentSection/AnimatedCard.js
@@ -0,0 +1,9 @@
+function AnimatedCards() {
+  return (
+    <div style={{ height: "100px", width: "100px", backgroundColor: "red" }}>
+      456
+    </div>
+  );
+}
+
+export default AnimatedCards;
